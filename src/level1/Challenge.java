4d3b380f9c437adb17af5bb6bbbd07a4364cd761
zot00 <league@WTS-iMac-11.attlocal.net>
2017-04-15 11:51:43 -0700

    made robot go through maze

--- src/level1/Challenge.java
+++ src/level1/Challenge.java
@@ -18,9 +18,16 @@ public class Challenge extends Head {
 	 */
 	public void initialize() {
 		//write code here!
-		
-		
-		
+		driveDirect(100,100);
+		sleep(13000);
+		driveDirect(-100, 100);
+		sleep(1900);
+		driveDirect(1000,1000);
+		sleep(3500);
+		driveDirect(-100, 100);
+		sleep(1800);
+		driveDirect(1000,1000);
+		sleep(13000);
 	}
 
 	public void loop() {
